8a5a01c1ca09dd452174bbeea442331daf6ec2ad
Simon Zimmermann <mail@simonzimmermann.de>
2025-02-05 17:01:01 +0100

    update gitignore

--- seele/resources/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/simonzimmermann/dev/seele/seele/resources
-# Build directory: /Users/simonzimmermann/dev/seele/seele/resources
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
